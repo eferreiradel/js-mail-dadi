f6a8c546328df165422f7b9245bdbb7e663b6ba5
eferreiradel <cont.limaferreira.everton@gmail.com>
2023-04-19 11:04:31 +0200

    feat: [JS]: ciclo for loop con variabile globale

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
+let accessAlowed = false;
+
 function sendEmail() {
   let permittedEmail = ["mail@1", "mail@2", "mail@3", "mail@4"];
 
   let userEmail = document.getElementById("userEmail").value;
 
-  let accessAlowed = false;
   for (let counter = 0; counter < permittedEmail.length; counter++) {
     let mailLIst = permittedEmail[counter];
 
     if (userEmail == mailLIst) {
-      alert("ok");
+      accessAlowed = true;
     }
+  }
+  if (accessAlowed == true) {
+    alert("Welcome");
+  } else {
+    alert("Not welcome");
   }
 }
